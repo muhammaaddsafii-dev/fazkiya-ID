ba2b1d3297a66a5cf51b65d5fe564ab185786c71
muhammaaddsafii-dev <76186341+muhammaaddsafii-dev@users.noreply.github.com>
2024-07-27 13:23:45 +0700

    add-chart

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use App\Models\User;
+use App\Models\Product;
+use App\Models\Testimonial;
+
+class StatsOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        $userCount = User::count();
+        $productCount = Product::count();
+        $testimonialCount = Testimonial::count();
+
+        return [
+            Stat::make('Jumlah User', $userCount)
+                ->description('Total pengguna di sistem')
+                ->descriptionIcon('heroicon-o-users')
+                ->color('success'),
+            Stat::make('Jumlah Produk', $productCount)
+                ->description('Total produk yang tersedia')
+                ->descriptionIcon('heroicon-o-shopping-cart')
+                ->color('warning'),
+            Stat::make('Jumlah Testimonial', $testimonialCount)
+                ->description('Total testimonial yang dikumpulkan')
+                ->descriptionIcon('heroicon-o-hand-thumb-up')
+                ->color('success'),
+        ];
+    }
+}
